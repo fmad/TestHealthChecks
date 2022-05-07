25ce63ada721e0978cc9343ea79570b756c10d2e
Mad <fmad@github.com>
2022-05-08 00:10:55 +0100

    Added expected value sper check Preparing for DNS Lookups Simplified code Removed HideCodes

--- Debug.ps1
+++ Debug.ps1
@@ -7,7 +7,14 @@ foreach( $code in '200','201','301','302','400','403','404','500','521' ) {
         Name    = "T:$code"
         Proto   = 'https'
         Address = "httpstat.us/$code"
+        Expected= @($code)
     }
+}
+$checks += @{
+    Name    = "T:404b"
+    Proto   = 'https'
+    Address = "httpstat.us/$code"
+    Expected= 200
 }
 $checks += @{
     Name    = 'T:IP'
@@ -20,22 +27,33 @@ $checks += @{
     Proto   = 'tcp'
     Address = (Resolve-DnsName -Name www.google.com -Type A).IP4Address
     Port    = 80
+    Expected= 0
 }
 $checks += @{
     Name    = 'TCP81'
     Proto   = 'tcp'
     Address = (Resolve-DnsName -Name www.google.com -Type A).IP4Address
     Port    = 81
+    Expected= -1
 }
 $checks += @{
     Name    = 'ICMP1'
     Proto   = 'icmp'
     Address = (Resolve-DnsName -Name www.google.com -Type A).IP4Address
+    Expected= 0
 }
 $checks += @{
     Name    = 'ICMP2'
     Proto   = 'icmp'
     Address = '255.255.255.255'
+    Expected= 11010
+}
+$checks += @{
+    Name    = 'DNS1'
+    Proto   = 'dns'
+    Address = '8.8.8.8'
+    Query   = 'MX:madruga.eu'
+    Expected= @('alt1.aspmx.l.google.com','alt2.aspmx.l.google.com','aspmx.l.google.com','aspmx2.googlemail.com','aspmx3.googlemail.com')
 }
 $now = (Get-Date).ToString('yyyy.MM.dd-HH.mm.ss')
 $logFolder = 'C:\Log'
@@ -45,11 +63,11 @@ if ( -not (Test-Path -Path $logFolder) ) {
 $errFile = "$logFolder\Err-debug-$now.txt"
 Write-Host "Errors will be saved to $errFile"
 $parms = @{
-    NoReturn        = $true
     Checks          = $checks
-    HideCodes       = $null # @(200)
     ErrorLogFile    = $errFile
+    SuccessErrorOnly= $true     # Only show success (0) or error (-1) if the result code is in the Expected list, otherwise display the code
     HideDuplicates  = $false
+    NoReturn        = $true
     TimeoutMiliSecs =  5000
     WaitMiliSecs    = 10000
 }
--- TestHealthChecksCmdlets.cs
+++ TestHealthChecksCmdlets.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -8,41 +9,26 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
+using DnsClient;
 
 namespace TestHealthChecks
 {
     [Cmdlet(VerbsDiagnostic.Test, "HealthChecks")]
     public class TestHealthChecksCmdlets: Cmdlet
     {
+        #region Parameters
+        [Parameter(Mandatory=true )] public Checks[]        Checks           { get; set; }
+        [Parameter(Mandatory=false)] public String          ErrorLogFile     { get; set; }
+        [Parameter(Mandatory=false)] public SwitchParameter HideDuplicates   { get; set; }
+        [Parameter(Mandatory=false)] public SwitchParameter NoReturn         { get; set; }
+        [Parameter(Mandatory=false)] public String          OutputLogFile    { get; set; }
+        [Parameter(Mandatory=false)] public int             RepeatHeader     { get; set; } = 20;
+        [Parameter(Mandatory=false)] public bool            SuccessErrorOnly { get; set; } = false;
+        [Parameter(Mandatory=false)] public int             TimeoutMiliSecs  { get; set; } = 2000;
+        [Parameter(Mandatory=false)] public int             WaitMiliSecs     { get; set; } = 100;
+        #endregion
+
         private bool _exitRequested = false;
-
-        [Parameter]
-        public int TimeoutMiliSecs { get; set; } = 2000;
-
-        [Parameter]
-        public int WaitMiliSecs { get; set; } = 100;
-
-        [Parameter]
-        public int RepeatHeader { get; set; } = 20;
-
-        [Parameter]
-        public SwitchParameter NoReturn { get; set; }
-
-        [Parameter]
-        public Checks[] Checks { get; set; }
-
-        [Parameter]
-        public int?[] HideCodes { get; set; }
-        
-        [Parameter]
-        public SwitchParameter HideDuplicates { get; set; }
-
-        [Parameter]
-        public String ErrorLogFile { get; set; }
-
-        [Parameter]
-        public String OutputLogFile { get; set; }
-
         private const int TimeOutCode = -100;
 
         protected override void StopProcessing()
@@ -53,23 +39,23 @@ namespace TestHealthChecks
         protected override void EndProcessing()
         {
             base.EndProcessing();
-            Stopwatch stopWatch = new Stopwatch();
-            var columns = Checks.Select(x => x.Name).ToList();
-            columns.Sort();
-            WriteVerbose($"Test-HealthChecks w/ {TimeoutMiliSecs}ms Timeout, {WaitMiliSecs}ms Wait, {(NoReturn.IsPresent?"-NoReturn":"")} and {columns.Count} checks to test...");
+            var    dnsClient    = new LookupClient();
+            var    stopWatch    = new Stopwatch();
             var    results      = new ConcurrentDictionary<String, Int32>();
             bool   repeat       = NoReturn.IsPresent;
             int    repeatHeader = RepeatHeader;
             string previousLine = null; // Used with "HideDuplicates" to avoid repeating like lines and forcing good results out of screen
             string currentLine  = null; // To hold the "current" line that would be output
+            var columns = Checks.Select(x => x.Name).ToList();
+            columns.Sort();
+            WriteVerbose($"Test-HealthChecks w/ {TimeoutMiliSecs}ms Timeout, {WaitMiliSecs}ms Wait, {(NoReturn.IsPresent ? "-NoReturn" : "")} and {columns.Count} checks to test...");
             try {
                 do {
                     stopWatch.Reset();
                     stopWatch.Start();
                     var timestamp = new DateTimeOffset(DateTime.UtcNow).ToString("yyyy/MM/dd HH:mm:ss.fff");
-                    PSObject obj = new PSObject();
+                    var obj       = new PSObject();
                     obj.Properties.Add(new PSNoteProperty("Timestamp", timestamp));
-
                     using (var finished = new CountdownEvent(1)) {
                         foreach (var test in columns) {
                             var check    = Checks.Where(x => x.Name == test).FirstOrDefault();  // Used to capture the loop variable in the lambda expression.
@@ -77,51 +63,33 @@ namespace TestHealthChecks
                             var proto    = check.Proto;
                             var port     = check.Port;
                             var hostname = check.Hostname;
-                            switch(proto) {
-                                case ProtoEnum.http:
-                                case ProtoEnum.https:
-                                    finished.AddCount(); // Indicate that there is another work item.
-                                    ThreadPool.QueueUserWorkItem(
-                                        (state) => {
-                                            try {
-                                                results[check.Name] = InvokeRequest($"{proto}://{address}", hostname);
-                                            } finally {
-                                                finished.Signal(); // Signal that the work item is complete.
-                                            }
-                                        }, null
-                                    );
-                                    break;
-                                case ProtoEnum.icmp:
-                                    finished.AddCount();
-                                    ThreadPool.QueueUserWorkItem(
-                                        (state) => {
-                                            try
-                                            {
-                                                results[check.Name] = TestIcmpConnect(address);
-                                            }
-                                            finally
-                                            {
-                                                finished.Signal();
-                                            }
-                                        }, null
-                                    );
-                                    break;
-                                case ProtoEnum.tcp:
-                                    finished.AddCount();
-                                    ThreadPool.QueueUserWorkItem(
-                                        (state) => {
-                                            try
-                                            {
-                                                results[check.Name] = TestTcpConnect(address,port);
-                                            } finally {
-                                                finished.Signal();
-                                            }
-                                        }, null
-                                    );
-                                    break;
-                                default:
-                                    throw new Exception("Unhandled Protocol");
-                            }
+                            var expected = check.Expected;
+                            finished.AddCount(); // Indicate that there is another work item.
+                            ThreadPool.QueueUserWorkItem(
+                                (state) => {
+                                    try {
+                                        switch (proto) {
+                                            case ProtoEnum.http:
+                                            case ProtoEnum.https:
+                                                results[check.Name] = InvokeRequest($"{proto}://{address}", hostname, expected);
+                                                break;
+                                            case ProtoEnum.icmp:
+                                                results[check.Name] = TestIcmpConnect(address, expected);
+                                                break;
+                                            case ProtoEnum.tcp:
+                                                results[check.Name] = TestTcpConnect(address, port, expected);
+                                                break;
+                                            case ProtoEnum.dns:
+                                                results[check.Name] = 0; // Do some DNS checks here...
+                                                break;
+                                            default:
+                                                throw new Exception("Unhandled Protocol");
+                                        }
+                                    } finally {
+                                        finished.Signal(); // Signal that the work item is complete.
+                                    }
+                                }, null
+                            );
                         }
                         finished.Signal(); // Signal that queueing is complete.
                         finished.Wait();   // Wait for all work items to complete.
@@ -130,9 +98,6 @@ namespace TestHealthChecks
                     foreach (var k in columns) {
                         int code = Int32.Parse(results[k].ToString());
                         string codeTxt = code != TimeOutCode ? code.ToString() : "---";
-                        if (null != HideCodes && HideCodes.Contains(code)) {
-                            codeTxt = "";
-                        }
                         currentLine += (currentLine.Length > 0 ? "," : "") + codeTxt;
                         obj.Properties.Add(new PSNoteProperty(k.ToString(), codeTxt));
                         if (ErrorLogFile != null && codeTxt.Trim() != "" && codeTxt != "200") {
@@ -171,37 +136,50 @@ namespace TestHealthChecks
             }
         }
 
-        protected int TestIcmpConnect(string Address)
+        protected int TestIcmpConnect(string Address, List<string>Expected)
         {
+            int result;
             try {
                 var pingSender = new Ping();
                 var reply = pingSender.Send(Address, TimeoutMiliSecs);
-                if (reply.Status == IPStatus.Success) {
-                    return 200;
+                result = (int)reply.Status;
+            } catch (Exception) {
+                result = (int)IPStatus.Unknown;
+            }
+            if (SuccessErrorOnly && Expected?.Count > 0) {
+                if (Expected.Contains(result.ToString())) {
+                    result = 0;
                 } else {
-                    return (int)reply.Status;
+                    result = -1;
                 }
-            } catch (Exception ex) {
-                return -1;
             }
+            return result;
         }
 
-        protected int TestTcpConnect(string Address, int Port) {
-            TcpClient tcpClient   = new TcpClient();
-            // future optimization: resolve address at startup
-            IPAddress ipAddress = Dns.GetHostEntry(Address).AddressList[0];
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, Port);
-            var result  = tcpClient.BeginConnect(Address, Port, null, null);
-            var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromMilliseconds(TimeoutMiliSecs));
+        protected int TestTcpConnect(string Address, int Port, List<string> Expected) {
+            var tcpClient  = new TcpClient();  // future optimization: resolve address at startup
+            var ipAddress  = Dns.GetHostEntry(Address).AddressList[0];
+            var ipEndPoint = new IPEndPoint(ipAddress, Port);
+            var conResult  = tcpClient.BeginConnect(Address, Port, null, null);
+            var success    = conResult.AsyncWaitHandle.WaitOne(TimeSpan.FromMilliseconds(TimeoutMiliSecs));
+            int result;
             if (success) {
-                tcpClient.EndConnect(result);
-                return 200;
+                tcpClient.EndConnect(conResult);
+                result = 0;
             } else {
-                return -1;
+                result = -1;
             }
+            if (SuccessErrorOnly && Expected?.Count > 0) {
+                if (Expected.Contains(result.ToString())) {
+                    result = 0;
+                } else {
+                    result = -1;
+                }
+            }
+            return result;
         }
 
-        protected int InvokeRequest(string URL, string Hostname)
+        protected int InvokeRequest(string URL, string Hostname, List<string> Expected)
         {
             var request = (HttpWebRequest)WebRequest.Create(URL);
             request.Timeout = TimeoutMiliSecs;
@@ -211,29 +189,38 @@ namespace TestHealthChecks
             request.AllowAutoRedirect = false;
             request.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore);
             int statusCode;
+            int result;
             try {
                 var response = request.GetResponse();
                 statusCode   = (int)((HttpWebResponse)response)?.StatusCode;
                 response.Close();
-                return statusCode;
+                result = statusCode;
             } catch (WebException e) {
                 if (e.Status == WebExceptionStatus.ProtocolError) {
                     if (e.Response is HttpWebResponse response) {
-                        return (int)response.StatusCode;
+                        result = (int)response.StatusCode;
                     } else {
-                        return TimeOutCode - 1; // no http status code available
+                        result = TimeOutCode - 1; // no http status code available
                     }
                 } else if (e.Status == WebExceptionStatus.Timeout) {
-                    return TimeOutCode; // Timeout
+                    result = TimeOutCode; // Timeout
                 } else {
-                    return -(int)e.Status; // Other exception
+                    result = -(int)e.Status; // Other exception
                 }
             } catch (Exception e2) {
                 using (StreamWriter w = File.AppendText("C:\\Debug\\err.txt")) {
                     w.WriteLine(e2.Message);
                 }
-                return TimeOutCode - 2; // Other exception
+                result = TimeOutCode - 2; // Other exception
             }
+            if (SuccessErrorOnly && Expected?.Count > 0) {
+                if (Expected.Contains(result.ToString())) {
+                    result = 0;
+                } else {
+                    result = -1;
+                }
+            }
+            return result;
         }
     }
 
@@ -242,14 +229,17 @@ namespace TestHealthChecks
         https = 0,
         http,
         icmp,
-        tcp
+        tcp,
+        dns
     }
     public class Checks
     {
-        public string    Name;
-        public string    Address;
-        public string    Hostname;
-        public int       Port;
-        public ProtoEnum Proto;
+        public string        Name;
+        public string        Address;
+        public string        Hostname;
+        public string        Query;
+        public int           Port;
+        public ProtoEnum     Proto;
+        public List<string>  Expected;
     }
 }
